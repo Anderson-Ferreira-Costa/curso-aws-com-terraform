c25f87e8d58ed7c7d0358ba4fef0d235ae26ff66
Anderson Ferreira <andersonferreiracosta@outlook.com.br>
2022-07-21 17:59:20 -0300

    primeiro commit

--- 01-terraform-basico/01-primeiro-script/main.tf
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "0.14.4"
+  # required_version = "0.14.4"
 
   required_providers {
     aws = {
@@ -11,18 +11,18 @@ terraform {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
-  region  = "eu-central-1" # Brasil -> us-east-1
-  profile = "tf014"
+  region  = "us-east-1" # Brasil -> us-east-1
+  profile = "default"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-123123455745642342342"
+  bucket = "andersonbatistaferreiracosta"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-    Managedby   = "Terraform"
+    ManagedBy   = "Terraform"
   }
 }
