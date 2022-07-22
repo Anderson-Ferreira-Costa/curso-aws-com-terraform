2aaed643fcaa3b5fcb5a4cac37850d40103e6347
Anderson Ferreira <andersonferreiracosta@outlook.com>
2022-07-22 13:03:47 -0300

    OK

--- 01-terraform-basico/03-variaveis/variables.tf
+++ 01-terraform-basico/03-variaveis/variables.tf
@@ -1,26 +1,31 @@
 variable "environment" {
   type        = string
   description = ""
+  default = "DEV"
 }
 
 variable "aws_region" {
   type        = string
   description = ""
+  default = "us-east-1"
 }
 
 variable "aws_profile" {
   type        = string
   description = ""
+  default = "default"
 }
 
 variable "instance_ami" {
   type        = string
   description = ""
+  default = "ami-052efd3df9dad4825"
 }
 
 variable "instance_type" {
   type        = string
   description = ""
+  default = "t2.micro"
 }
 
 variable "instance_tags" {
